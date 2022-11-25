d28d9294e301009b8982a3984f89a9ea27ff8498
Roberto Petit de Meurville <rpetit@riu.com>
2022-11-24 21:22:38 -0300

    particles try 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +7,5 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   title: string;
-
-  constructor() {
-  }
 
 }
